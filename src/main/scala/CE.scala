894d74a557d8c9587685b462784b26822b9cc039
Xiaohui Li <lixiaohui@bwrcrdsl-6.EECS.Berkeley.EDU>
2016-03-31 20:39:09 -0700

    updated CE.scala

--- src/main/scala/CE.scala
+++ src/main/scala/CE.scala
@@ -19,46 +19,6 @@ case class CEParams {
     width: DSPUInt=5
 }
 
-//create shift Reg group to save Inputs for width of time (a param for max width)
-// width is how many data/co-eff used to determine , n is the dat length
-class ShiftReg (width:Int, n:Int) extends Module {
-  val io = new Bundle {
-    val ins = Vec.fill(width){UInt(INPUT,n)}
-    val load = Bool(INPUT)
-    val shift = Bool(INPUT)
-    val out = Vec.fill(width){UInt(OUTPUT,n)}
-  }
-  val delays = Vec.fill(width){ Reg(UInt()) }
-  when (io.load) {
-    for (i <- 0 until width){
-      delays(i) := io.ins(i)
-    }.elsewhen (io.shift) {
-      delays(0) := io.ins(0)
-      for (i <- 1 until width){
-        delays(i) := delays(i-1)
-      }
-    }
-  }
-  io.out := delays
-}
-// the output is the whole vec, can be used to store the weight as well
-// create several vec to store: original data, original PT, current weight (all length:width)
-
-// creat reg to save the weight for each PT
-class SaveReg (width:Int, n:Int) extends Module {
-  val io = new Bundle {
-    val ins = Vec.fill(width){UInt(INPUT,n)}
-    val load = Bool(INPUT)
-    val out = Vec.fill(width){UInt(OUTPUT,n)}
-  }
-  val stored = Vec.fill(width){ Reg(UInt()) }
-  when (io.load) {
-      stored := io.ins
-      }
-  io.out := stored
-}
-
-
 class CE[T <: DSPQnm[T]](gen : => T) extends GenDSPModule (gen) {
 //class CE(implicit params: CEParams) extends GenDSPModule (gen) {
 
@@ -70,78 +30,51 @@ class CEIO [T <: DSPQnm[T]](gen : => T) extends IOBundle {
 }
 
   val  pt_number = ceil(params.frame_size/pt_position) //division ?
-  val  stored_In= (0 until params.frame_size).map(i => Module(new ShiftReg(width = params.width, n = params.data_size)))
-  val  stored_Weight= (0 until pt_number).map(i => Module(new SaveReg(width = params.width, n = params.data_size)))
-  val tmp_in = Vec.fill(params.width){ Reg(UInt()) }
-  val tmp_weight = Vec.fill(params.width){ Reg(UInt()) }
+  val  stored_Weight= (0 until pt_number).map(i => Reg(init = DSPUInt(1, width = data_size)))
+  val tmp_weight = Reg(init = DSPUInt(1, width = data_size))
 
 //determine whether is pilot tone using the position comb type
-  val sigCount = DSPUInt(0, frame_size)
-  val PTCount = DSPUInt(0, frame_size)
+  val sigCount = Reg(init = DSPUInt(0, width = data_size)
+  val PTCount = Reg(init = DSPUInt(0, width = data_size)
   val IsPT = DSPBool(false) //no need here, but needed for other PT position
   if (sigCount !== frame_size ){
       sigCount := sigCount + DSPUInt(1)
-      stored_In(sigCount).io.ins(0) := signalIn.real.toInt  // no need toInt?
-      stored_In(sigCount).io.shift := DSPBool (true)  // store input to the shiftreg
       if (sigCount%p.pt_position == DSPUInt(1) ){ //input is a PT
       IsPT := DSPBool(true)
       PTcount := PTcount + DSPUInt(1)
       //do calculation here
-      tmp_in := stored_In(sigCount)
       tmp_weight := stored_Weight(PTCount)
-      val dot_product := Reg(init=DSPUInt(0,width = params.data_size)) //need to truncate to data_size?
-      for (i <- 0 until params.width){
-        dot_product := dot_product + tmp_in(i) * tmp_weight(i) // get the dot product using weight and input
-      } 
-      signalOut_real := dot_product //if the input is PT assign the dot product to output and update weight
-      val error = params.pt_value - dot_product
-      val f = Vec.fill(params.width){ Reg(UInt()) }
-      f = mu * error * tmp_in
-      store_Weight(PTCount) := alpha * tmp_weight + f //update whole vector?
+      signalOut_real := tmp_weight * signalIn.real.toUInt // toUInt?
+      val error = params.pt_value - signalOut_real
+      f = mu * error * signalIn.real.toUInt
+      store_Weight(PTCount) := alpha * tmp_weight + f 
       }.otherwise{
       IsPT := DSPBool(false)
-      tmp_in := stored_In(sigCount)
+      PTcount := PTcount + DSPUInt(1)
+      //do calculation here
       tmp_weight := stored_Weight(PTCount)
-      val dot_product := Reg(init=DSPUInt(0,width = params.data_size)) //need to truncate to data_size?
-      for (i <- 0 until params.width){
-        dot_product := dot_product + tmp_in(i) * tmp_weight(i) // get the dot product using weight and input
-      } 
-      signalOut_real := dot_product //if the input is not PT, assign the dot product to output
+      signalOut_real := tmp_weight * signalIn.real.toUInt // toUInt?
       }
   }.otherwise{
       sigCount := DSPUInt(0) //reset the counter for a new frame
       PTCount := DSPUInt(0)
       // same thing 
-      stored_In(sigCount).io.ins(0) := signalIn.real.toInt  // no need toInt?
-      stored_In(sigCount).io.shift := DSPBool (true)  // store input to the shiftreg
-      if (sigCount%p.pt_position == DSPUInt(1) ){
+      if (sigCount%p.pt_position == DSPUInt(1) ){ //input is a PT
       IsPT := DSPBool(true)
       PTcount := PTcount + DSPUInt(1)
       //do calculation here
-      tmp_in := stored_In(sigCount)
       tmp_weight := stored_Weight(PTCount)
-      val dot_product := Reg(init=DSPUInt(0,width = params.data_size)) //need to truncate to data_size?
-      for (i <- 0 until params.width){
-        dot_product := dot_product + tmp_in(i) * tmp_weight(i) // get the dot product using weight and input
-      } 
-      signalOut_real := dot_product //if the input is PT assign the dot product to output and update weight
-      val error = params.pt_value - dot_product
-      val f = Vec.fill(params.width){ Reg(UInt()) }
-      f = mu * error * tmp_in
-      store_Weight(PTCount) := alpha * tmp_weight + f //update whole vector?
+      signalOut_real := tmp_weight * signalIn.real.toUInt // toUInt?
+      val error = params.pt_value - signalOut_real
+      f = mu * error * signalIn.real.toUInt
+      store_Weight(PTCount) := alpha * tmp_weight + f 
       }.otherwise{
       IsPT := DSPBool(false)
-      tmp_in := stored_In(sigCount)
+      PTcount := PTcount + DSPUInt(1)
+      //do calculation here
       tmp_weight := stored_Weight(PTCount)
-      val dot_product := Reg(init=DSPUInt(0,width = params.data_size)) //need to truncate to data_size?
-      for (i <- 0 until params.width){
-        dot_product := dot_product + tmp_in(i) * tmp_weight(i) // get the dot product using weight and input
+      signalOut_real := tmp_weight * signalIn.real.toUInt // toUInt?
       } 
-      signalOut_real := dot_product //if the input is not PT, assign the dot product to output
-      }
-    }
-
-
-}
+   }
 
 }
