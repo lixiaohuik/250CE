7f5fc7a7938bbaee348bd4e74650251553a0a405
Xiaohui Li <lixiaohui@berkeley.edu>
2016-04-07 19:09:31 -0700

    compiled CE.scala

--- src/main/scala/CE.scala
+++ src/main/scala/CE.scala
@@ -11,62 +11,70 @@ import ChiselDSP._
 // assum have the pilot tone first?
 
 class CEParams {
-    var mu:		Double	= 1.0
-    var alpha:		Double	= 1.0
-    var pt_position: 	DSPUInt	= ChiselDSP.DSPUInt(3)
-    var frame_size: 	DSPUInt = ChiselDSP.DSPUInt(20)
-    var data_size: 	DSPUInt = ChiselDSP.DSPUInt(2)
-    var pt_value_r: 	DSPUInt = ChiselDSP.DSPUInt(2)
-    var pt_value_i: 	DSPUInt = ChiselDSP.DSPUInt(2)
-    var width: 		DSPUInt = ChiselDSP.DSPUInt(5)
+    
+  var mu:		Double	= 1.0
+  var alpha:		Double	= 1.0
+  var pt_position: 	Int	= 4
+  var frame_size: 	Int 	= 20
+  var min_value:	Double  = -255.0			// DSPFixed uses min value to determine bit width rather than actual bit width
+  var max_value: 	Double 	= 255.0				// DSPFixed uses max value to determine bit width rather than actual bit width
+  var frac_width:	Int	= 32				// DSPFixed has extra argument for fraction width
+
+  // At some point I'd like to make width > 1 so the pt_values should be vectors rather than one value once that happens
+  var pt_value_r: 	DSPFixed = DSPFixed(1.0, frac_width)	
+  var pt_value_i: 	DSPFixed = DSPFixed(1.0, frac_width)
+  var width: 		Int	= 5			// Currently unused since width is 1
+
 }
 
 class CE[T <: DSPQnm[T]](gen : => T, params : => CEParams) extends GenDSPModule (gen) {
-//class CE(implicit params: CEParams) extends GenDSPModule (gen) {
 
-class CEIO [T <: DSPQnm[T]](gen : => T) extends IOBundle {
-//input signal  
-  val signalIn = Complex(gen).asInput
-  val signalOut_real = DSPUInt (OUTPUT, params.data_size )
-  val signalOut_imag = DSPUInt (OUTPUT, params.data_size )
+class CEIO [T <: DSPQnm[T]](gen : => T, params : CEParams) extends IOBundle {
+
+  val signalIn_real = DSPFixed(INPUT, params.frac_width, (params.min_value, params.max_value))
+  val signalIn_imag = DSPFixed(INPUT, params.frac_width, (params.min_value, params.max_value))
+  val signalOut_real = DSPFixed(OUTPUT, params.frac_width, (params.min_value, params.max_value))
+  val signalOut_imag = DSPFixed(OUTPUT, params.frac_width, (params.min_value, params.max_value))
+
 }
+  override val io = new CEIO(gen, params) 
 
-  val  pt_number = math.ceil(params.frame_size/params.pt_position) //division ?
-  val  stored_Weight_r= (0 until pt_number).map(i => Reg(init = DSPUInt(1, width = data_size)))
-  val  stored_Weight_i= (0 until pt_number).map(i => Reg(init = DSPUInt(1, width = data_size)))
-  val tmp_weight_r = Reg(init = DSPUInt(1, width = data_size))
-  val tmp_weight_i = Reg(init = DSPUInt(1, width = data_size))
-
+  val pt_number = (math.ceil(params.frame_size.toDouble/params.pt_position.toDouble)).toInt 
+  val stored_Weight_r = Vec.fill(pt_number){RegInit(DSPFixed(1.0, params.frac_width))}
+  val stored_Weight_i = Vec.fill(pt_number){RegInit(DSPFixed(1.0, params.frac_width))}
+  val tmp_weight_r = DSPFixed(1.0, params.frac_width)
+  val tmp_weight_i = DSPFixed(1.0, params.frac_width)
+ 
 //determine whether is pilot tone using the position comb type
-  val sigCount = Reg(init = DSPUInt(0, width = data_size))
-  val PTCount = Reg(init = DSPUInt(0, width = data_size))
-  val IsPT = DSPBool(false) //no need here, but needed for other PT position
-  if (sigCount !== frame_size ){
-      sigCount := sigCount + DSPUInt(1)
-      }.otherwise{
-          sigCount := DSPUInt(0)
-          PTCount := DSPUInt(0)
-      }
-  if (sigCount%p.pt_position == DSPUInt(1) ){ //input is a PT
-      IsPT := DSPBool(true)
-      PTcount := PTcount + DSPUInt(1)
-      //do calculation here
-      tmp_weight_r := stored_Weight_r(PTCount)
-      tmp_weight_i := stored_Weight_i(PTCount)
-      signalOut_real := tmp_weight_r * signalIn.real.toUInt // toUInt?
-      signalOut_imag := tmp_weight_i * signalIn.imag.toUInt // toUInt?
-      val error_r = params.pt_value_r - signalOut_real
-      val error_i = params.pt_value_i - signalOut_imag
-      f_r = params.mu * error_r * signalIn.real.toUInt
-      f_i = params.mu * error_i * signalIn.imag.toUInt
-      store_Weight_r(PTCount) := alpha * tmp_weight_r + f_r 
-      store_Weight_i(PTCount) := alpha * tmp_weight_i + f_i 
-      }.otherwise{
-          IsPT := DSPBool(false)
-          //do calculation here
-          tmp_weight_r := stored_Weight_r(PTCount)
-          tmp_weight_i := stored_Weight_i(PTCount)
-          signalOut_real := tmp_weight_r * signalIn.real.toUInt // toUInt?
-          signalOut_imag := tmp_weight_i * signalIn.imag.toUInt // toUInt?
-      }
+  val sigCount = UInt(0)
+  val PTCount = UInt(0)
+  val IsPT = Bool(false) //no need here, but needed for other PT position
+  when (sigCount =/= UInt(params.frame_size) ){
+    sigCount := sigCount + UInt(1)
+  }.otherwise{
+    sigCount := UInt(0)
+    PTCount := UInt(0)
+  }
+  when (sigCount%UInt(params.pt_position) === UInt(1) ){ //input is a PT
+    IsPT := Bool(true)
+    PTCount := PTCount + UInt(1)
+    //do calculation here
+    tmp_weight_r := stored_Weight_r(PTCount)
+    tmp_weight_i := stored_Weight_i(PTCount)
+    io.signalOut_real := tmp_weight_r * io.signalIn_real
+    io.signalOut_imag := tmp_weight_i * io.signalIn_imag
+    val error_r = params.pt_value_r - io.signalOut_real
+    val error_i = params.pt_value_i - io.signalOut_imag
+    val f_r = DSPFixed(params.mu, params.frac_width) * error_r * io.signalIn_real
+    val f_i = DSPFixed(params.mu, params.frac_width) * error_i * io.signalIn_imag
+    stored_Weight_r(PTCount) := DSPFixed(params.alpha, params.frac_width) * tmp_weight_r + f_r
+    stored_Weight_i(PTCount) := DSPFixed(params.alpha, params.frac_width) * tmp_weight_i + f_i
+  }.otherwise{
+    IsPT := Bool(false)
+    //do calculation here
+    tmp_weight_r := stored_Weight_r(PTCount)
+    tmp_weight_i := stored_Weight_i(PTCount)
+    io.signalOut_real := tmp_weight_r * io.signalIn_real
+    io.signalOut_imag := tmp_weight_i * io.signalIn_imag
+  }
 }
