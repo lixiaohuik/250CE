aaae4f62772a23ff482ccf97bb3d87e4d13075ba
Xiaohui Li <lixiaohui@berkeley.edu>
2016-04-11 15:29:47 -0700

    for debug

--- src/main/scala/CE.scala
+++ src/main/scala/CE.scala
@@ -21,13 +21,15 @@ case class CEParams (
    frac_width:	Int	= 32,				// DSPFixed has extra argument for fraction width,
 
   // At some point I'd like to make width > 1 so the pt_values should be vectors rather than one value once that happens
-   pt_value_r: 	DSPFixed = DSPFixed(1.0, 32),	
-   pt_value_i: 	DSPFixed = DSPFixed(1.0, 32),
+   //pt_value_r: 	DSPFixed = DSPFixed(1.0, 32),	
+   //pt_value_i: 	DSPFixed = DSPFixed(1.0, 32),
+   pt_value_r: 	Double = 1.0, 	
+   pt_value_i: 	Double = 1.0, 
    width: 		Int	= 5			// Currently unused since width is 1
 
 )
 
-class CE[T <: DSPQnm[T]](gen : => T, p : => CEParams) extends GenDSPModule (gen) {
+class CE[T <: DSPQnm[T]](gen : => T, p : CEParams) extends GenDSPModule (gen) {
 
 class CEIO [T <: DSPQnm[T]](gen : => T, p : CEParams) extends IOBundle {
 
@@ -44,7 +46,10 @@ class CEIO [T <: DSPQnm[T]](gen : => T, p : CEParams) extends IOBundle {
   val stored_Weight_i = Vec.fill(pt_number){RegInit(DSPFixed(1.0, p.frac_width))}
   val tmp_weight_r = DSPFixed(1.0, p.frac_width)
   val tmp_weight_i = DSPFixed(1.0, p.frac_width)
- 
+
+  val pt_value_r = DSPFixed(p.pt_value_r, p.frac_width)
+  val pt_value_i = DSPFixed(p.pt_value_i, p.frac_width)
+
 //determine whether is pilot tone using the position comb type
   val sigCount = UInt(0)
   val PTCount = UInt(0)
@@ -59,12 +64,14 @@ class CEIO [T <: DSPQnm[T]](gen : => T, p : CEParams) extends IOBundle {
     IsPT := Bool(true)
     PTCount := PTCount + UInt(1)
     //do calculation here
+    println(stored_Weight_r(0))// print the vec
     tmp_weight_r := stored_Weight_r(PTCount)
+    println(tmp_weight_r)// print the assigned
     tmp_weight_i := stored_Weight_i(PTCount)
     io.signalOut_real := tmp_weight_r * io.signalIn_real
     io.signalOut_imag := tmp_weight_i * io.signalIn_imag
-    val error_r = p.pt_value_r - io.signalOut_real
-    val error_i = p.pt_value_i - io.signalOut_imag
+    val error_r = pt_value_r - io.signalOut_real
+    val error_i = pt_value_i - io.signalOut_imag
     val f_r = DSPFixed(p.mu, p.frac_width) * error_r * io.signalIn_real
     val f_i = DSPFixed(p.mu, p.frac_width) * error_i * io.signalIn_imag
     stored_Weight_r(PTCount) := DSPFixed(p.alpha, p.frac_width) * tmp_weight_r + f_r
