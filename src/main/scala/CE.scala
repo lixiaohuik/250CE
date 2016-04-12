7d71fcce9f24e70f48e6a50b72b826b9bbcad8b9
Xiaohui Li <lixiaohui@berkeley.edu>
2016-04-12 13:28:28 -0700

    DEBUG

--- src/main/scala/CE.scala
+++ src/main/scala/CE.scala
@@ -19,6 +19,7 @@ case class CEParams (
    min_value:	Double  = -255.0,			// DSPFixed uses min value to determine bit width rather than actual bit width,
    max_value: 	Double 	= 255.0,				// DSPFixed uses max value to determine bit width rather than actual bit width,
    frac_width:	Int	= 32,				// DSPFixed has extra argument for fraction width,
+   frac_width_big:	BigInt	= 16,				// DSPFixed has extra argument for fraction width,
 
   // At some point I'd like to make width > 1 so the pt_values should be vectors rather than one value once that happens
    //pt_value_r: 	DSPFixed = DSPFixed(1.0, 32),	
@@ -44,15 +45,13 @@ class CEIO [T <: DSPQnm[T]](gen : => T, p : CEParams) extends IOBundle {
   val pt_number = (math.ceil(p.frame_size.toDouble/p.pt_position.toDouble)).toInt 
   val stored_Weight_r = Vec.fill(pt_number){RegInit(DSPFixed(1.0, p.frac_width))}
   val stored_Weight_i = Vec.fill(pt_number){RegInit(DSPFixed(1.0, p.frac_width))}
-  val tmp_weight_r = DSPFixed(1.0, p.frac_width)
-  val tmp_weight_i = DSPFixed(1.0, p.frac_width)
 
   val pt_value_r = DSPFixed(p.pt_value_r, p.frac_width)
   val pt_value_i = DSPFixed(p.pt_value_i, p.frac_width)
 
 //determine whether is pilot tone using the position comb type
-  val sigCount = UInt(0)
-  val PTCount = UInt(0)
+  val sigCount = RegInit(UInt(0,width = 32))
+  val PTCount =  RegInit(UInt(0,width = 32))
   val IsPT = Bool(false) //no need here, but needed for other PT position
   when (sigCount =/= UInt(p.frame_size) ){
     sigCount := sigCount + UInt(1)
@@ -64,24 +63,39 @@ class CEIO [T <: DSPQnm[T]](gen : => T, p : CEParams) extends IOBundle {
     IsPT := Bool(true)
     PTCount := PTCount + UInt(1)
     //do calculation here
-    println(stored_Weight_r(0))// print the vec
-    tmp_weight_r := stored_Weight_r(PTCount)
-    println(tmp_weight_r)// print the assigned
-    tmp_weight_i := stored_Weight_i(PTCount)
-    io.signalOut_real := tmp_weight_r * io.signalIn_real
-    io.signalOut_imag := tmp_weight_i * io.signalIn_imag
+//    println(stored_Weight_r(0))// print the vec
+    val tmp_weight_r = stored_Weight_r(PTCount)
+//    println(tmp_weight_r)// print the assigned
+    val tmp_weight_i = stored_Weight_i(PTCount)
+    val signalOut_real_long = tmp_weight_r * io.signalIn_real
+    val signalOut_imag_long = tmp_weight_i * io.signalIn_imag
+//    println(io.signalOut_real.getRange)
+    //io.signalOut_real := signalOut_real_long.shorten(io.signalOut_real.getRange)
+    //io.signalOut_imag := signalOut_imag_long.shorten(io.signalOut_imag.getRange)
+           io.signalOut_real := signalOut_real_long $$
+           //io.signalOut_real := signalOut_real_long.shorten(p.frac_width_big,p.frac_width_big)
+           //io.signalOut_imag := signalOut_imag_long.shorten(p.frac_width)
     val error_r = pt_value_r - io.signalOut_real
     val error_i = pt_value_i - io.signalOut_imag
     val f_r = DSPFixed(p.mu, p.frac_width) * error_r * io.signalIn_real
     val f_i = DSPFixed(p.mu, p.frac_width) * error_i * io.signalIn_imag
-    stored_Weight_r(PTCount) := DSPFixed(p.alpha, p.frac_width) * tmp_weight_r + f_r
-    stored_Weight_i(PTCount) := DSPFixed(p.alpha, p.frac_width) * tmp_weight_i + f_i
-  }.otherwise{
+    val stored_Weight_r_long = DSPFixed(p.alpha, p.frac_width) * tmp_weight_r + f_r
+    val stored_Weight_i_long = DSPFixed(p.alpha, p.frac_width) * tmp_weight_i + f_i
+    //stored_Weight_r(PTCount) := stored_Weight_r_long.shorten(tmp_weight_r.getRange)
+    //stored_Weight_i(PTCount) := stored_Weight_i_long.shorten(tmp_weight_i.getRange)
+    //println(stored_Weight_r_long)
+    //stored_Weight_r(PTCount) := stored_Weight_r_long.shorten(p.frac_width)
+    //stored_Weight_i(PTCount) := stored_Weight_i_long.shorten(p.frac_width)
+  }.otherwise {
     IsPT := Bool(false)
     //do calculation here
-    tmp_weight_r := stored_Weight_r(PTCount)
-    tmp_weight_i := stored_Weight_i(PTCount)
-    io.signalOut_real := tmp_weight_r * io.signalIn_real
-    io.signalOut_imag := tmp_weight_i * io.signalIn_imag
+    val tmp_weight_r = stored_Weight_r(PTCount)
+    val tmp_weight_i = stored_Weight_i(PTCount)
+    val signalOut_real_long = tmp_weight_r * io.signalIn_real
+    val signalOut_imag_long = tmp_weight_i * io.signalIn_imag
+    //io.signalOut_real := signalOut_real_long.shorten(io.signalOut_real.getRange)
+    //io.signalOut_imag := signalOut_imag_long.shorten(io.signalOut_imag.getRange)
+    //io.signalOut_real := signalOut_real_long.shorten(p.frac_width)
+    //io.signalOut_imag := signalOut_imag_long.shorten(p.frac_width)
   }
 }
