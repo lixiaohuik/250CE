e23499150aa33b35f91fee5f01325215a3848e10
Xiaohui Li <lixiaohui@berkeley.edu>
2016-04-13 22:27:19 -0700

    clean but the width error again

--- src/main/scala/CE.scala
+++ src/main/scala/CE.scala
@@ -43,7 +43,6 @@ class CEIO [T <: DSPQnm[T]](gen : => T, p : CEParams) extends IOBundle {
 
 }
   override val io = new CEIO(gen, p) 
-
   val pt_number = (math.ceil(p.frame_size.toDouble/p.pt_position.toDouble)).toInt 
   val stored_Weight_r = Vec.fill(pt_number){RegInit(double2T(1.0, p.frac_width))}
   val stored_Weight_i = Vec.fill(pt_number){RegInit(double2T(1.0, p.frac_width))}
@@ -54,86 +53,69 @@ class CEIO [T <: DSPQnm[T]](gen : => T, p : CEParams) extends IOBundle {
 //determine whether is pilot tone using the position comb type
   val sigCount = RegInit(UInt(0,width = (math.ceil(math.log(p.frame_size)*4)).toInt))
   val PTCount =  RegInit(UInt(0,width = (math.ceil(math.log(pt_number)*4)).toInt))
-  val sigPosition =  RegInit(UInt(1,width = (math.ceil(math.log(p.pt_position)*4)).toInt))  //Indicate the signal position in a ( PT DATA) block. so if sigPosition is equal to pt_position, it's a PT, other wise it's the position of the signal, the initial 1 should be assigned to 1 again after the first cycle
+  val sigPosition =  RegInit(UInt(0,width = (math.ceil(math.log(p.pt_position)*4)).toInt))  //Indicate the signal position in a ( PT DATA) block. so if sigPosition is equal to pt_position, it's a PT, other wise it's the position of the signal, the initial 1 should be assigned to 1 again after the first cycle
 //    println(sigCount)
   val IsPT = Bool(false) //no need here, but needed for other PT position
-  
+// assign output with LMS algorithm  
+//tmp_weight is determined by interpolation method
+//reset counter for each frame TODO: or use the output from FFT to indicate the start of each frame  
+
+  val tmp_weight_r  = double2T(1.0, p.frac_width)
+  val tmp_weight_i  = double2T(1.0, p.frac_width)
+
   when (sigCount =/= UInt(p.frame_size)){
     sigCount := sigCount + UInt(1)
   }.otherwise{
     sigCount := UInt(0)
-    PTCount := UInt(0)
-    sigPosition := UInt(0)
   }
- 
-  //when (sigCount%UInt(p.pt_position) === UInt(1) ) //input is a PT
-  when (UInt(sigPosition) === (UInt(p.pt_position)-UInt(1))  || (sigCount === UInt(0))){ //input is a PT
+  
+//when (sigCount%UInt(p.pt_position) === UInt(1) ) //input is a PT
+  when (UInt(sigPosition) === (UInt(p.pt_position)-UInt(1))  || (sigCount === UInt(0))){ //input is a PT TODO: assume 1st signal is always PT, so need to add offset
     IsPT := Bool(true)
-    PTCount := PTCount + UInt(1)
+    PTCount := PTCount + UInt(1)  //TODO:if the last signal in this fram is PT willcause problem
     sigPosition := UInt(0)
-    //do calculation here
-//    println(stored_Weight_r(0))// print the vec
-    val tmp_weight_r = stored_Weight_r(PTCount)
-//    println(tmp_weight_r)// print the assigned
-    val tmp_weight_i = stored_Weight_i(PTCount)
-    val signalOut_real_long = tmp_weight_r * io.signalIn_real
-    val signalOut_imag_long = tmp_weight_i * io.signalIn_imag
-    val temp_real_O = signalOut_real_long $(io.signalOut_real.getFracWidth)
-    val temp_imag_O = signalOut_imag_long $(io.signalOut_imag.getFracWidth)
-    io.signalOut_real := temp_real_O.shorten(io.signalOut_real.getRange)
-    io.signalOut_imag := temp_imag_O.shorten(io.signalOut_imag.getRange)
-    
+    tmp_weight_r := stored_Weight_r(PTCount)
+    tmp_weight_i := stored_Weight_i(PTCount)
+    //do calculation here to update weight
     val error_r = pt_value_r - io.signalOut_real
-    val error_i = pt_value_i - io.signalOut_imag
     val f_r = double2T(p.mu, p.frac_width) * error_r * io.signalIn_real
-    val f_i = double2T(p.mu, p.frac_width) * error_i * io.signalIn_imag
- 
     val stored_Weight_r_long = double2T(p.alpha, p.frac_width) * tmp_weight_r + f_r
-    val stored_Weight_i_long = double2T(p.alpha, p.frac_width) * tmp_weight_i + f_i
-   
     val temp_real_W = stored_Weight_r_long $(tmp_weight_r.getFracWidth)
-    
     stored_Weight_r(PTCount) := temp_real_W.shorten(stored_Weight_r(PTCount).getRange)
+          //same thing for imag 
+    val error_i = pt_value_i - io.signalOut_imag
+    val f_i = double2T(p.mu, p.frac_width) * error_i * io.signalIn_imag
+    val stored_Weight_i_long = double2T(p.alpha, p.frac_width) * tmp_weight_i + f_i
     val temp_imag_W = stored_Weight_i_long $(tmp_weight_r.getFracWidth)
     stored_Weight_i(PTCount) := temp_imag_W.shorten(stored_Weight_i(PTCount).getRange)
-  }.otherwise {
+  }.otherwise {      // input is not PT, just update with stored weight 
+  			//but depends on interpolation method
     IsPT := Bool(false)
     sigPosition := sigPosition + UInt(1)
-    //do calculation here
+    // 
         when (UInt(p.interp) === UInt(0)){
-            val tmp_weight_r = stored_Weight_r(PTCount)
-            val tmp_weight_i = stored_Weight_i(PTCount)
 
-            val signalOut_real_long = tmp_weight_r * io.signalIn_real
-            val signalOut_imag_long = tmp_weight_i * io.signalIn_imag
-            val temp_real_O = signalOut_real_long $(io.signalOut_real.getFracWidth)
-            val temp_imag_O = signalOut_imag_long $(io.signalOut_imag.getFracWidth)
-            io.signalOut_real := temp_real_O.shorten(io.signalOut_real.getRange)
-            io.signalOut_imag := temp_imag_O.shorten(io.signalOut_imag.getRange)
+    tmp_weight_r := stored_Weight_r(PTCount)
+    tmp_weight_i := stored_Weight_i(PTCount)
         }.otherwise {
+    tmp_weight_r := stored_Weight_r(PTCount)
+    tmp_weight_i := stored_Weight_i(PTCount)
 //            val next_w = sigPosition/UInt(p.pt_position)
 //            val current_w = UInt(1) - next_w
 //	    println (current_w)
 //            val tmp_weight_r = stored_Weight_r(PTCount)*current_w
 	     // +stored_Weight_r(PTCount + UInt(1)) * next_w
-            val tmp_weight_i = stored_Weight_i(PTCount)
+            //val tmp_weight_i = stored_Weight_i(PTCount)
 
-//          val signalOut_real_long = tmp_weight_r * io.signalIn_real
-            val signalOut_imag_long = tmp_weight_i * io.signalIn_imag
-//     //     println (signalOut_real_long)
-//     //       val temp_real_O = signalOut_real_long $(p.frac_width)
-            val temp_imag_O = signalOut_imag_long $(io.signalOut_imag.getFracWidth)
-       //     //io.signalOut_real := temp_real_O.shorten(io.signalOut_real.getRange)
-            io.signalOut_imag := temp_imag_O.shorten(io.signalOut_imag.getRange)
         }
-//        println (test)
-//        println (test.getFracWidth)
-//        println (test.getIntWidth)
 //        println (io.signalOut_real.getFracWidth)
 //        println (io.signalOut_real.getIntWidth)
-        //io.signalOut_real := signalOut_real_long.shorten(io.signalOut_real.getRange)
-        //io.signalOut_imag := signalOut_imag_long.shorten(io.signalOut_imag.getRange)
-        //io.signalOut_real := signalOut_real_long.shorten(p.frac_width)
-        //io.signalOut_imag := signalOut_imag_long.shorten(p.frac_width)
   }
+
+  val signalOut_real_long = tmp_weight_r * io.signalIn_real
+  val signalOut_imag_long = tmp_weight_i * io.signalIn_imag
+  val temp_real_O = signalOut_real_long $(io.signalOut_real.getFracWidth)
+  val temp_imag_O = signalOut_imag_long $(io.signalOut_imag.getFracWidth)
+  io.signalOut_real := temp_real_O.shorten(io.signalOut_real.getRange)
+  io.signalOut_imag := temp_imag_O.shorten(io.signalOut_imag.getRange)
 }
