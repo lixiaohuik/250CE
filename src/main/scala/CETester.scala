7fe1835a23ef9ff59011b702bfcf94fd6be847db
Xiaohui Li <lixiaohui@berkeley.edu>
2016-04-22 17:08:08 -0700

    fixed tester

--- src/main/scala/CETester.scala
+++ src/main/scala/CETester.scala
@@ -16,8 +16,9 @@ val i_test_input = Source.fromFile("dirty_i.txt").getLines.toArray
 val r_writer = new PrintWriter(new File("r_output.txt"))
 val i_writer = new PrintWriter(new File("i_output.txt"))
 
-
-for (i <- 0 until r_test_input.length){
+peek (c.sigCount)
+step(1)
+for (i <- 0 until i_test_input.length){
           poke(c.io.signalIn_real, r_test_input(i).toDouble)
           poke(c.io.signalIn_imag, i_test_input(i).toDouble)
           peek (c.sigCount)
@@ -28,12 +29,12 @@ for (i <- 0 until r_test_input.length){
           peek (c.celms.io.new_weight_r)
 	  var r_output:Double = peek(c.io.signalOut_real)
 	  var i_output:Double = peek(c.io.signalOut_imag)
-	  r_writer.write(r_output.toString +"\n")
-	  i_writer.write(i_output.toString +"\n")
+//	  r_writer.write(r_output.toString +"\n")
+//	  i_writer.write(i_output.toString +"\n")
 	  step(1)
 	  }
-r_writer.close()
-i_writer.close()
+//r_writer.close()
+//i_writer.close()
 
 //poke(c.io.signalIn_real, 2.0)
 //poke(c.io.signalIn_imag, 2.0)
