9411c257300d92ebdc93caeb78db321069575f0c
Xiaohui Li <lixiaohui@berkeley.edu>
2016-04-24 23:20:08 -0700

    algorithm, interpolation, pipeline stages Options

--- src/main/scala/CE.scala
+++ src/main/scala/CE.scala
@@ -13,8 +13,8 @@ import scala.collection.mutable.ListBuffer
 // assum have the pilot tone first?
 
 case class CEParams (
-   interp:      Int     = 1,                   // 0: step , 1 : linear    
-   algorithm:   Int     = 0,                   // 0: lms, 1: sign
+   interp:      Int     = 0,                   // 0: step , 1 : linear    
+   algorithm:   Int     = 1,                   // 0: lms, 1: sign
    mu:		Double	= 0.05,                 //step size
    alpha:	Double	= 1.0,                 //coefficient of older weight
    pt_position: Int	= 2,                   // # of signals between PT + 1 (pitch?)
@@ -23,6 +23,7 @@ case class CEParams (
    max_value: 	Double 	= 127.0,				// DSPFixed uses max value to determine bit width rather than actual bit width,
    frac_width:	Int	= 20,				// DSPFixed has extra argument for fraction width,
    int_width:	Int	= 3,				// DSPFixed has extra argument for fraction width,
+   pipeline:	Int	= 2,		       // 0 for no pipeline, 1 for 1 pipeline
 
   // At some point I'd like to make width > 1 so the pt_values should be vectors rather than one value once that happens
    //pt_value_r: 	DSPFixed = DSPFixed(1.0, 32),	
@@ -45,10 +46,7 @@ class LMS[T <: DSPQnm[T]](gen : => T, p : CEParams) extends GenDSPModule (gen) {
     }
   override val io = new LMSIO(gen, p)
 
-    
-  //  val stored_Weight_r_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_r 
-  //  val stored_Weight_i_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_i 
-   
+  if (p.pipeline == 0 || p.pipeline == 1) {
     if (p.algorithm == 0){//lms
         val f_r_lms = double2T(p.mu, (p.int_width,p.frac_width)) * io.error_r * io.signalIn_real
         val f_i_lms = double2T(p.mu, (p.int_width,p.frac_width)) * io.error_i * io.signalIn_imag
@@ -59,8 +57,6 @@ class LMS[T <: DSPQnm[T]](gen : => T, p : CEParams) extends GenDSPModule (gen) {
         io.new_weight_i := (stored_Weight_i_long $ io.new_weight_i.getFracWidth).shorten(io.new_weight_i.getRange)
       }
       else{//error sign
-        //val error_sign_r = Mux(io.error_r > double2T(0), double2T(1) , double2T(-1, (p.int_width,p.frac_width)))
-        //val error_sign_i = Mux(io.error_i > double2T(0), double2T(1) , double2T(-1, (p.int_width,p.frac_width)))
         when(io.error_r > double2T(0)){
 	  val f_r_sign = double2T(p.mu, (p.int_width,p.frac_width)) * io.signalIn_real 
           val stored_Weight_r_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_r + f_r_sign
@@ -80,9 +76,52 @@ class LMS[T <: DSPQnm[T]](gen : => T, p : CEParams) extends GenDSPModule (gen) {
         io.new_weight_i := (stored_Weight_i_long $ io.new_weight_i.getFracWidth).shorten(io.new_weight_i.getRange)
 	}
       }
+    } else if (p.pipeline == 2) {
+//pipeline
+    if (p.algorithm == 0){//lms
+        val f_r_lms_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+        val f_i_lms_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+        val f_r_lms = double2T(p.mu, (p.int_width,p.frac_width)) * io.error_r * io.signalIn_real
+        val f_i_lms = double2T(p.mu, (p.int_width,p.frac_width)) * io.error_i * io.signalIn_imag
+        when(Bool(true)){
+          f_r_lms_reg := (f_r_lms $ f_r_lms_reg.getFracWidth)
+          f_i_lms_reg := (f_i_lms $ f_i_lms_reg.getFracWidth)
+        }
+
+        val stored_Weight_r_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_r + f_r_lms_reg
+        val stored_Weight_i_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_i + f_i_lms_reg
+        io.new_weight_r := (stored_Weight_r_long $ io.new_weight_r.getFracWidth).shorten(io.new_weight_r.getRange)
+        io.new_weight_i := (stored_Weight_i_long $ io.new_weight_i.getFracWidth).shorten(io.new_weight_i.getRange)
+      }
+      else{//error sign
+        val f_r_sign_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+        val f_i_sign_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+        
+	when(io.error_r > double2T(0)){
+	  val f_r_sign = double2T(p.mu, (p.int_width,p.frac_width)) * io.signalIn_real 
+          f_r_sign_reg := (f_r_sign $ f_r_sign_reg.getFracWidth)
+          val stored_Weight_r_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_r + f_r_sign_reg
+        io.new_weight_r := (stored_Weight_r_long $ io.new_weight_r.getFracWidth).shorten(io.new_weight_r.getRange)
+	}.otherwise{
+	  val f_r_sign = double2T(p.mu, (p.int_width,p.frac_width)) * io.signalIn_real 
+          f_r_sign_reg := (f_r_sign $ f_r_sign_reg.getFracWidth)
+          val stored_Weight_r_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_r - f_r_sign_reg
+        io.new_weight_r := (stored_Weight_r_long $ io.new_weight_r.getFracWidth).shorten(io.new_weight_r.getRange)
+	}
+        when(io.error_i > double2T(0)){
+	  val f_i_sign = double2T(p.mu, (p.int_width,p.frac_width)) * io.signalIn_imag
+          f_i_sign_reg := (f_i_sign $ f_i_sign_reg.getFracWidth)
+          val stored_Weight_i_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_i + f_i_sign_reg
+        io.new_weight_i := (stored_Weight_i_long $ io.new_weight_i.getFracWidth).shorten(io.new_weight_i.getRange)
+	}.otherwise{
+	  val f_i_sign = double2T(p.mu, (p.int_width,p.frac_width)) * io.signalIn_imag 
+          f_i_sign_reg := (f_i_sign $ f_i_sign_reg.getFracWidth)
+          val stored_Weight_i_long = double2T(p.alpha,(p.int_width, p.frac_width)) * io.cur_weight_i - f_i_sign_reg
+        io.new_weight_i := (stored_Weight_i_long $ io.new_weight_i.getFracWidth).shorten(io.new_weight_i.getRange)
+	}
+      }
+  }
     
-    //io.new_weight_r := (stored_Weight_r_long $ io.new_weight_r.getFracWidth).shorten(io.new_weight_r.getRange)
-    //io.new_weight_i := (stored_Weight_i_long $ io.new_weight_i.getFracWidth).shorten(io.new_weight_i.getRange)
 }
 
 class EQ[T <: DSPQnm[T]](gen : => T, p : CEParams) extends GenDSPModule (gen) {
@@ -154,8 +193,42 @@ class CE[T <: DSPQnm[T]](gen : => T, p : CEParams) extends GenDSPModule (gen) {
 //for linear interpolation
   val sec_weight_r = stored_Weight_r(PTCount+UInt(1))
   val sec_weight_i = stored_Weight_i(PTCount+UInt(1))
-//  val sec_weight_r = stored_Weight_r(PTCount)
-//  val sec_weight_i = stored_Weight_i(PTCount)
+
+  // pipeline registers
+  val signalIn_real_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+  val signalIn_imag_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+  val prev_weight_real_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+  val prev_weight_imag_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+  val eq_out_real_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+  val eq_out_imag_reg = RegInit(double2T(1.0, (p.int_width,p.frac_width)))
+  val PT_reg = RegInit(UInt(1,width = (math.ceil(math.log(pt_number+1)*4)).toInt))
+  val sig_pos_reg = RegInit(UInt(0,width = (math.ceil(math.log(p.pt_position+1)*4)).toInt))
+
+  // second set of pipeline registers  
+  val PT_reg2 = RegInit(UInt(1,width = (math.ceil(math.log(pt_number+1)*4)).toInt))
+  val sig_pos_reg2 = RegInit(UInt(0,width = (math.ceil(math.log(p.pt_position+1)*4)).toInt))
+
+  // if pipelining
+  if (p.pipeline > 0){
+    when(Bool(true)){
+      signalIn_real_reg := io.signalIn_real
+      signalIn_imag_reg := io.signalIn_imag
+      prev_weight_real_reg := cur_weight_r
+      prev_weight_imag_reg := cur_weight_i
+      eq_out_real_reg := ceeq.io.signalOut_real
+      eq_out_imag_reg := ceeq.io.signalOut_imag
+      PT_reg := PTCount  
+      sig_pos_reg := sigPosition
+    }
+  }
+
+  if (p.pipeline == 2) {
+    when(Bool(true)){  
+      PT_reg2 := PT_reg
+      sig_pos_reg2 := sig_pos_reg
+    }
+  }
+
 
   val error_r = pt_value_r - io.signalOut_real
   val error_i = pt_value_i - io.signalOut_imag
@@ -163,67 +236,78 @@ class CE[T <: DSPQnm[T]](gen : => T, p : CEParams) extends GenDSPModule (gen) {
   ceeq.io.signalIn_real := io.signalIn_real
   ceeq.io.weight_r := (cur_weight_r $ ceeq.io.weight_r.getFracWidth).shorten(ceeq.io.weight_r.getRange)
   ceeq.io.weight_r_2 := (sec_weight_r $ ceeq.io.weight_r_2.getFracWidth).shorten(ceeq.io.weight_r_2.getRange)
-  io.signalOut_real := ceeq.io.signalOut_real
 
   ceeq.io.signalIn_imag := io.signalIn_imag
   ceeq.io.weight_i := (cur_weight_i $ ceeq.io.weight_i.getFracWidth).shorten(ceeq.io.weight_i.getRange)
   ceeq.io.weight_i_2 := (sec_weight_i $ ceeq.io.weight_i_2.getFracWidth).shorten(ceeq.io.weight_i_2.getRange)
-  io.signalOut_imag := ceeq.io.signalOut_imag
   
   ceeq.io.sigPosition := (sigPosition_dbl $ ceeq.io.sigPosition.getFracWidth).shorten(ceeq.io.sigPosition.getRange)//no double2T?
 
+  if (p.pipeline == 0) {
+    io.signalOut_real := ceeq.io.signalOut_real
+    io.signalOut_imag := ceeq.io.signalOut_imag
+  } else {
+    io.signalOut_real := eq_out_real_reg
+    io.signalOut_imag := eq_out_imag_reg
+  }
 
   //Connect lms all the time as well
   celms.io.error_r := (error_r $ celms.io.error_r.getFracWidth).shorten(celms.io.error_r.getRange)
-  celms.io.signalIn_real := io.signalOut_real
-  celms.io.cur_weight_r := (cur_weight_r $ celms.io.cur_weight_r.getFracWidth).shorten(celms.io.cur_weight_r.getRange)
-  
   celms.io.error_i := (error_i $ celms.io.error_i.getFracWidth).shorten(celms.io.error_i.getRange)
-  celms.io.signalIn_imag := io.signalOut_imag
-  celms.io.cur_weight_i := (cur_weight_i $ celms.io.cur_weight_i.getFracWidth).shorten(celms.io.cur_weight_i.getRange)
-
+  if (p.pipeline == 0) {
+    celms.io.signalIn_real := io.signalIn_real
+    celms.io.cur_weight_r := (cur_weight_r $ celms.io.cur_weight_r.getFracWidth).shorten(celms.io.cur_weight_r.getRange)
+  
+    celms.io.signalIn_imag := io.signalIn_imag
+    celms.io.cur_weight_i := (cur_weight_i $ celms.io.cur_weight_i.getFracWidth).shorten(celms.io.cur_weight_i.getRange)
+  } else {
+    celms.io.signalIn_real := signalIn_real_reg
+    celms.io.cur_weight_r := prev_weight_real_reg 
+  
+    celms.io.signalIn_imag := signalIn_imag_reg
+    celms.io.cur_weight_i := prev_weight_real_reg
+  }
+  
 debug (sigCount)
 debug (PTCount)
 debug (sigPosition)
 debug (error_r)
 
+  when (sigCount === UInt(p.frame_size)) {
+    sigCount := UInt(1)
+  }.otherwise {
+    sigCount := sigCount + UInt(1)
+  }
 
-  when (sigPosition === UInt(p.pt_position) ){ //next input is a PT
+  when (sigPosition === UInt(p.pt_position)) {
     sigPosition := UInt(1)
     sigPosition_dbl := double2T(1, (p.int_width,p.frac_width))
-    
+  }.otherwise {
+    sigPosition := sigPosition + UInt(1)
+    sigPosition_dbl := ((sigPosition_dbl + double2T(1.0)) $ p.frac_width).shorten(sigPosition_dbl.getRange)
+  }
+
+  when (sigCount === UInt(p.frame_size)) {
+    PTCount := UInt(1)
+  }.elsewhen (sigPosition === UInt(p.pt_position)) {
     PTCount := PTCount + UInt(1)
-          when (sigCount === UInt(p.frame_size)){
-            sigCount := UInt(1)
-            PTCount := UInt(1)
-          }.otherwise{
-            sigCount := sigCount + UInt(1)
-          }
-   }.elsewhen(sigPosition === UInt(1) ){
-    sigPosition := sigPosition + UInt(1)
-    sigPosition_dbl := ((sigPosition_dbl + double2T(1.0)) $ p.frac_width).shorten(sigPosition_dbl.getRange)
-   //update the weight here
-    stored_Weight_r(PTCount) := (celms.io.new_weight_r $ stored_Weight_r(0).getFracWidth).shorten(stored_Weight_r(0).getRange)
-    stored_Weight_i(PTCount) := (celms.io.new_weight_i $ stored_Weight_i(0).getFracWidth).shorten(stored_Weight_i(0).getRange)
-          when (sigCount === UInt(p.frame_size)){
-            sigCount := UInt(1)
-            PTCount := UInt(1)
-          }.otherwise{
-            sigCount := sigCount + UInt(1)
-          }
-   
-   }.otherwise {
-    sigPosition := sigPosition + UInt(1)
-    sigPosition_dbl := ((sigPosition_dbl + double2T(1.0)) $ p.frac_width).shorten(sigPosition_dbl.getRange)
-          when (sigCount === UInt(p.frame_size)){
-            sigCount := UInt(1)
-            PTCount := UInt(1)
-          }.otherwise{
-            sigCount := sigCount + UInt(1)
-          }
-   }
+  }
+  
+  if (p.pipeline == 0) {
+    when (sigPosition === UInt(1)) {
+      stored_Weight_r(PTCount) := (celms.io.new_weight_r $ stored_Weight_r(0).getFracWidth).shorten(stored_Weight_r(0).getRange)
+      stored_Weight_i(PTCount) := (celms.io.new_weight_i $ stored_Weight_i(0).getFracWidth).shorten(stored_Weight_i(0).getRange)
+    }
+  } else if (p.pipeline == 1){
+    when (sig_pos_reg === UInt(1)) {
+      stored_Weight_r(PT_reg) := (celms.io.new_weight_r $ stored_Weight_r(0).getFracWidth).shorten(stored_Weight_r(0).getRange)
+      stored_Weight_i(PT_reg) := (celms.io.new_weight_i $ stored_Weight_i(0).getFracWidth).shorten(stored_Weight_i(0).getRange)
+    }
+  } else if (p.pipeline == 2){
+    when (sig_pos_reg2 === UInt(1)) {
+      stored_Weight_r(PT_reg2) := (celms.io.new_weight_r $ stored_Weight_r(0).getFracWidth).shorten(stored_Weight_r(0).getRange)
+      stored_Weight_i(PT_reg2) := (celms.io.new_weight_i $ stored_Weight_i(0).getFracWidth).shorten(stored_Weight_i(0).getRange)
+    }
+  }
+
 }
-//        println (test.getFracWidth)
-//        println (test.getIntWidth)
-//        println (io.signalOut_real.getFracWidth)
-//        println (io.signalOut_real.getIntWidth)
